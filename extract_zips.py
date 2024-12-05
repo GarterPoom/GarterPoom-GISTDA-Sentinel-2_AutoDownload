1bc8616b7aed7ae44013ff17f1f54c0a0647c600
GarterPoom <siripoom.su@gmail.com>
2024-12-05 11:02:30 +0700

    Update Log Path

--- extract_zips.py
+++ extract_zips.py
@@ -7,7 +7,7 @@ import logging
 logging.basicConfig(level=logging.INFO, 
                     format='%(asctime)s - %(levelname)s: %(message)s',
                     handlers=[
-                        logging.FileHandler('Sentinel-2_extraction.log'),
+                        logging.FileHandler('extraction_log/Sentinel-2_extraction.log'),
                         logging.StreamHandler()
                     ])
 
